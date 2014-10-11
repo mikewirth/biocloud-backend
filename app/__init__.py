692517fc900c947359c88c5cc8664388e6828fbc
Igor Krawczuk <Igor.Krawczuk@TUM.de>
2014-10-11 03:30:43 +0200

    can now serve images returned by opencv

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,7 @@
-from flask import Flask
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+from flask import Flask,render_template
 
 app = Flask(__name__)
 from app import views
--- app/views.py
+++ app/views.py
@@ -1,10 +1,27 @@
 from app import app
+from flask import render_template,send_file
+import tempfile
+import cv2
+from StringIO import StringIO
+import os
+import Image
 
 @app.route('/')
 def index():
-    return "Hello World"
+    return render_template("index.html")
 
 
-@app.route('/render')
+@app.route('/render',methods=['POST'])
 def render():
     return "Hello World Render"
+
+@app.route('/sobelimg.bmp')
+def sobel():
+    img_io = StringIO()
+    #print os.getcwd()
+    lena = cv2.imread('app/static/lena.bmp',0)
+    p = cv2.Canny(lena,100,200)
+    p2 = Image.fromarray(p)
+    p2.save(img_io,'BMP')
+    img_io.seek(0)
+    return send_file(img_io,mimetype='image/bmp',attachment_filename='sobelimg.bmp',as_attachment=True)
