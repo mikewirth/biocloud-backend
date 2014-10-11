1592f43731e9681eafd282e69a2be14bd6fc727f
Igor Krawczuk <Igor.Krawczuk@TUM.de>
2014-10-11 04:18:36 +0200

    can also serve, just have to call app from /static/index.html and adjust routes accordingly

--- app/views.py
+++ app/views.py
@@ -8,7 +8,7 @@ import Image
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("/static/index.html")
 
 
 @app.route('/render',methods=['POST'])
@@ -20,8 +20,8 @@ def sobel():
     img_io = StringIO()
     #print os.getcwd()
     lena = cv2.imread('app/static/lena.bmp',0)
-    p = cv2.Canny(lena,100,200)
-    p2 = Image.fromarray(p)
-    p2.save(img_io,'BMP')
+    trafolena = cv2.Canny(lena,100,200)
+    im = Image.fromarray(trafolena)
+    im.save(img_io,'BMP')
     img_io.seek(0)
-    return send_file(img_io,mimetype='image/bmp',attachment_filename='sobelimg.bmp',as_attachment=True)
+    return send_file(img_io,mimetype='image/bmp',attachment_filename='does_not_matter.bmp',as_attachment=True)
