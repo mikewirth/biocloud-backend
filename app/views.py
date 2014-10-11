254ab859ae2a4221f641dd5c3698fa51d0d68f9b
Ubuntu <ubuntu@ip-172-31-2-122.eu-west-1.compute.internal>
2014-10-11 14:18:36 +0000

    adding analysis routes now

--- app/views.py
+++ app/views.py
@@ -25,17 +25,78 @@ def index():
 def test():
     return "Hello Wor"
 
+def gaussianBlur(img,blrSize=3):
+    return cv2.GaussianBlur(img,(blrSize,blrSize),0)
+	
+
+#treshhold the image after seprating it into subimages and counting the edges as a heuristic
+def filterBackgroundNoise(img,subimageDimX=30,subimageDimY=30):
+    for ix in range(img.shape[0]/subimageDimX) :
+        for iy in range(img.shape[1]/subimageDimY) :
+            # Extracting sub image
+            xStartPixel = ix*subimageDimX
+            yStartPixel = iy*subimageDimY
+            xEndPixel = ix*subimageDimX + subimageDimX
+            yEndPixel = iy*subimageDimY + subimageDimY
+            subImage = blr[xStartPixel:xEndPixel,yStartPixel:yEndPixel]
+            # Thresholding using Otsu
+            varianceThreshold = 10
+            if np.var(subImage.ravel()) > varianceThreshold :
+            	thresh,thresholdedImg = cv2.threshold(subImage,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+            else :
+            	thresholdedImg = 0*subImage
+            # Adding the subimage the result image
+            resultImage[xStartPixel:xEndPixel,yStartPixel:yEndPixel] = thresholdedImg    
+    return resultImage
+
+def noiseRemoval(img,kSize=3):
+    kernel = np.ones((kSize,kSize),np.uint8)
+    return cv2.morphologyEx(resultImage, cv2.MORPH_OPEN, kernel)
+
+#jsut reads from filesys now, change for upload later
+def getImg():
+    lena = cv2.imread('app/lena512.bmp',0)
+    return lena
+
+#use the specified transofrm on the image and return the result
+def transform(img,method,params):
+if (method == "gaussianBlur"):
+    return gaussianBlur(img,**params)
+elif (method =="noiseRemoval"):
+    return noiseRemoval(img,**params)
+elif (method== "filterBackgroundNoise"):
+    return filterBackgroundNoise(img,**params)
+
+@app.route('/analyze',methods=['POST','GET'])
+def analyze():
+	json = request.get_json(force=True) 	
 
 @app.route('/render',methods=['POST','GET'])
 @cross_origin()
 def render():
+    orig=getImg()
+    #get the image, for now just use one from the server
     img_io = StringIO()
-    lena = cv2.imread('app/lena512.bmp',0)
-    trafolena = cv2.Canny(lena,100,200)
-    im = Image.fromarray(trafolena)
-    im.save(img_io,'BMP')
+    #stringio for return of image without storing on disk
+    json = request.get_json(force=True)
+    #json request specifiyng the order of operations and at what point we return the result
+    intermediate=lena
+    for i in range(json["showUntil"]:
+        intermediate=transform(intermediate,**json["transformations"][i])
+    result = Image.fromarray(intermediate)
+    result.save(img_io,'BMP')
     img_io.seek(0)
     return send_file(img_io,mimetype='image/bmp',attachment_filename='does_not_matter.bmp',as_attachment=True)
+
+def analyze(img,transformations,analysis):
+    pass
+
+@app.route('/batch')
+def batch():
+    #get list from json
+    #get transformations from json
+    #get analysis to be done from json
+
 
 @app.route('/sobelimg.bmp')
 def sobel():
