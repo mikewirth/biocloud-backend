f1c6caf5fe7ffa6b02b84f98b98a4e53943ed0cf
Prageeth Jayathissa <biocloud@richard-stockton.dreamhost.com>
2014-10-11 02:29:03 -0700

    cors should work now, moving servers

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,14 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 from flask import Flask,render_template
+from flask.ext.cors import CORS
 
 app = Flask(__name__)
+# Set CORS options on app configuration
+app.config['CORS_HEADERS'] = "Origin, X-Requested-With,Content-Type, Accept"
+app.config['PROPAGATE_EXCEPTIONS']=True
+app.config['CORS_RESOURCES'] = {r"/render": {"origins": "localhost:3000"}}
+
+cors = CORS(app)
+
 from app import views
--- app/views.py
+++ app/views.py
@@ -1,27 +1,35 @@
 from app import app
-from flask import render_template,send_file
+from flask import render_template,send_file,request
 import tempfile
-import cv2
+from flask.ext.cors import cross_origin
+#import cv2
 from StringIO import StringIO
 import os
-import Image
+#import Image
 
 @app.route('/')
 def index():
     return render_template("/static/index.html")
 
+@app.route('/test')
+def test():
+    return "Hello Wor"
 
-@app.route('/render',methods=['POST'])
+
+@app.route('/render',methods=['POST','GET'])
+@cross_origin()
 def render():
-    return "Hello World Render"
+    img_io = StringIO()
+    return send_file("lena.bmp",mimetype='image/bmp',attachment_filename='does_not_matter.bmp',as_attachment=True)
 
 @app.route('/sobelimg.bmp')
 def sobel():
     img_io = StringIO()
+    im = open('lena.bmp','rb')
     #print os.getcwd()
-    lena = cv2.imread('app/static/lena.bmp',0)
-    trafolena = cv2.Canny(lena,100,200)
-    im = Image.fromarray(trafolena)
+#    lena = cv2.imread('app/static/lena.bmp',0)
+ #   trafolena = cv2.Canny(lena,100,200)
+ #   im = Image.fromarray(trafolena)
     im.save(img_io,'BMP')
     img_io.seek(0)
     return send_file(img_io,mimetype='image/bmp',attachment_filename='does_not_matter.bmp',as_attachment=True)
